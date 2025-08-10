7c912f705ffd474e07ed496b176c232b357f9712
Angel <angeldavidtrillodelacruz@gmail.com>
2025-08-10 12:59:00 -0500

    actualizarLista corregido y sortearAmigo Terminado,

--- app.js
+++ app.js
@@ -7,25 +7,36 @@ const entradaAmigo = document.getElementById('amigo');
 const listaAmigos = document.getElementById('listaAmigos');
 const salidaResultado = document.getElementById('resultado');
 
-function agregarAmigo(){
+function agregarAmigo() {
   let nombreAmigo = entradaAmigo.value.trim();
 
-if(entradaAmigo.value === ''){
-  alert("Porfavor, Inserte un nombre");
-}else{
-  amigos.push(nombreAmigo);
-  entradaAmigo.value = '';
-}
-actualizarLista();
-}
-function actualizarLista(){
-  amigos.innerHTML = "";
-  for(let i = 0; i < amigos.length; i++){
-  let item = document.createElement("li");
-  item.innerHTML = amigos[i];
-  listaAmigos.appendChild(item);  
+  if (entradaAmigo.value === '') {
+    alert("Por favor, Inserte un nombre.");
+  } else {
+    amigos.push(nombreAmigo);
+    entradaAmigo.value = '';
   }
-  
+  actualizarLista();
+}
+
+//actualizar lista corregido.
+function actualizarLista() {
+  listaAmigos.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li");
+    item.innerHTML = amigos[i];
+    listaAmigos.appendChild(item);
   }
-  
+
+}
+
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Agrega al menos a un amigo antes de sortear.");
+  } else {
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let ganador = amigos[indiceAleatorio];
+    salidaResultado.innerHTML = ` El amigo secreto es ${ganador}`;
+  }
+}
 
